4294059c2ea3c1cda234606ab5d7d959be6a25b3
Jesper Brännström <jesper.brannstrom@ffcg.se>
2013-05-13 14:04:34 +0200

    Here comes the first commit.

---
+++ TrainingApp/Models/TrainingProgram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrainingApp.Models
+{
+    public class TrainingProgram
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string TypeOfProgram { get; set; }
+        public int CycleLengthInWeeks { get; set; }
+    }
+}
